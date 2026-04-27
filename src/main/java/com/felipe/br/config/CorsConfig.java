901038a994ea0bfdf805854902a0428f9f8757b2
Franca28 <felipe.oliveira.dev2025@gmail.com>
2026-04-27 20:02:26 -0300

    Reset do vite

--- src/main/java/com/felipe/br/config/CorsConfig.java
+++ src/main/java/com/felipe/br/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-		.allowedOrigins("http://localhost:8080")
-		.allowedMethods("POST", "GET", "PUT", "DELETE");
+		.allowedOrigins("http://localhost:5173")
+		.allowedMethods("POST", "GET", "PUT", "DELETE")
+		.allowedHeaders("*");
 	}
 }
