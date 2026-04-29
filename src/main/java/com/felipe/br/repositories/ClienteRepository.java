7a2f9c220867cf24d604d686c98208f287df6460
Felipe Feliciano <felipe.oliveira.dev2025@gmail.com>
2026-04-29 09:21:17 -0300

    update ClienteRepository (de novo novo novo novo)

--- src/main/java/com/felipe/br/repositories/ClienteRepository.java
+++ src/main/java/com/felipe/br/repositories/ClienteRepository.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.felipe.br.entities.Cliente;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 	
-	@Query("SELECT c FROM Cliente c LEFT JOIN FETCH c.pedidos WHERE LOWER(c.nome) = LOWER(:nome)")
+	@Query("SELECT DISTINCT c FROM Cliente c LEFT JOIN FETCH c.pedidos WHERE LOWER(c.nome) = LOWER(:nome)")
 	List<Cliente> findClienteByNome(@Param("nome") String nome);
 	
 	@Query("SELECT c FROM Cliente c JOIN FETCH c.pedidos WHERE c.id = :id")
